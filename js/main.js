cc68b34cc657ee167d0088cbb6eb733870106070
domrichichi <domrichichi@gmail.com>
2016-03-29 17:20:02 -0400

    Completed first box

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-var $btnshowhide = $('btn-show-hide');
+var $btnshowhide = $('.btn-show-hide');
 var $box = $('.box');
 
-var $btnmove = $('btn-move');
+var $btnmove = $('.btn-move');
+var $diamond = $('.diamond');
+
+// var $btnce = $('.btn-collapse-expand');
+// var $panel = $('.panel');
+
+// var $btnbounce = $('.btn-bounce');
+// var $circle = $('.circle');
+
+// var $btnappend = $('.btn-append');
+// var $list = $('.list');
+
+$btnshowhide.on('click', function() {
+  $box.toggleClass('show-box')
+});
+
+$btnmove.on('click', function() {
+  $diamond.toggleClass('')
+});
